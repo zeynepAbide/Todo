e7a8ffa377fa155152285bdfe175ab6d7a2c3577
Zeynepabide <zeynepabide@gmail.com>
2025-06-27 14:26:56 +0300

    checked unchecked eklendi

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,4 +1,4 @@
-import React ,{useRef, useState} from 'react'
+import React ,{useEffect, useRef, useState} from 'react'
 import todo from '../assets/todo.png'
 import Todoitems from './Todoitems'
 
@@ -33,6 +33,27 @@ const deleteTodo = (id)=> {
   })
 }
 
+const toggle = (id) =>{
+  setTodoList((prevTodos) =>{
+    return prevTodos.map((todo) =>{
+      if(todo.id === id){
+        return {...todo, isComplete: !todo.isComplete};
+    }
+
+    return todo;
+
+  })
+})
+}
+
+useEffect(()=>{
+  console.log(todoList);
+},[todoList])
+
+
+
+
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
       
@@ -49,7 +70,7 @@ const deleteTodo = (id)=> {
     <div className='flex items-center my-7 bg-gray-200 rounded-full'
     >
         <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='Add new task'/>
-        <button onClick={add} className='border-none rounded-full bg- w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD</button>
+        <button onClick={add} className='border-none rounded-full bg- w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
     </div>
 
 
@@ -58,7 +79,7 @@ const deleteTodo = (id)=> {
     <div>
 
     {todoList.map((item, index)=>{
-      return <Todoitems key={index}  text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}/>
+      return <Todoitems key={index}  text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
 
     })}
 
--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -3,12 +3,12 @@ import checked from '../assets/checked.png'
 import unchecked from '../assets/unchecked.png'
 import trash from '../assets/trash.png'
 
-const Todoitems = ({text, id, isComplete, deleteTodo}) => {
+const Todoitems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
     <div className='flex flex-row items-center my-3 gap-2'>
-    <div className='flex items-center cursor-pointer'>
-        <img src={checked} alt="" className='w-7'/>
-        <p className='text-slate-700 ml-4 text-[17px]'>
+    <div onClick={()=>{toggle(id) }} className='flex items-center cursor-pointer'>
+        <img src={isComplete ? checked : unchecked} alt="" className='w-7'/>
+        <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-950 ${isComplete ? "line-through" : ""}`}>
             {text}</p>
     </div>
 
