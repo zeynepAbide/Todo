85e38b9daf81b35f4964163ebe00a96d31f24a55
Zeynepabide <zeynepabide@gmail.com>
2025-06-27 11:05:46 +0300

    vite react installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ToDo from './components/ToDo'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid py-4 min-h-screen'>
+      <ToDo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import todo from '../assets/todo.png'
+import Todoitems from './Todoitems'
+
+const ToDo = () => {
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+      
+{/* ---title--- */}
+
+    <div className='flex items-center mt-7 gap-2'>
+        <img className='w-8' src={todo} alt="" />
+    <h1 className='text-xs font-semibold'>To-Do List</h1>
+
+    </div>
+
+
+{/* ---input box--- */}
+
+    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='Add new task'/>
+        <button className='border-none rounded-full bg-blue-600 w-32 h-14 text-white text-lg font-medium cursor-pointer-
+        '>ADD</button>
+    </div>
+
+
+{/* ----todo list----     */}
+
+    <div>
+        <Todoitems text ="Learn Coding"/>
+        <Todoitems text ="Done with Project"/>
+        
+    </div>
+
+
+    </div>
+  )
+}
+
+export default ToDo
